855a65c0595570198ca1dcb327e002a68cd2369a
Edison Mayorga <mayorga.sebas100@hotmail.com>
2024-09-19 19:10:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appTiendaUcompesar',
+  webDir: 'www'
+};
+
+export default config;
